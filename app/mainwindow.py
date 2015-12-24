80d0c31d14632bf3b4a5afaa88e406ede29de7f3
Mateusz Maćkowski <m4tx@m4tx.pl>
2015-12-24 15:40:52 +0100

    Add simple MainWindow

---
+++ app/mainwindow.py
@@ -0,0 +1,42 @@
+from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+
+from app.ui.ui_main import Ui_MainWindow
+
+ABOUT_HTML = ('<html><head/><body>'
+              '<p><span style=" font-size:18pt;">KrakSat 2016</span><br/>'
+              'Ground Station Software</p>'
+              '<p><a href="http://cansat.kraksat.pl">cansat.kraksat.pl</a></p>'
+              '<p>Copyright (c) 2015<br />'
+              'KrakSat Team in CanSat 2016</p>'
+              '</body></html>')
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    CONFIG_GEOMETRY_KEY = 'mainWindow/geometry'
+    CONFIG_STATE_KEY = 'mainWindow/state'
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        settings = get_settings()
+        self.restoreGeometry(settings.value(self.CONFIG_GEOMETRY_KEY))
+        self.restoreState(settings.value(self.CONFIG_STATE_KEY))
+
+        docks = (self.logsDock, self.queueDock, self.statisticsDock,
+                 self.cameraDock)
+        for dock in docks:
+            self.menuView.addAction(dock.toggleViewAction())
+
+    def show_about(self):
+        QMessageBox().about(self, 'About KrakSat 2016 Ground Station Software',
+                            ABOUT_HTML)
+
+    def closeEvent(self, QCloseEvent):
+        settings = get_settings()
+        settings.setValue(self.CONFIG_GEOMETRY_KEY, self.saveGeometry())
+        settings.setValue(self.CONFIG_STATE_KEY, self.saveState())
+
+
+def get_settings():
+    return QSettings('kraksat', 'receiver')
--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from app.login import LoginDialog
+from app.mainwindow import MainWindow
 
 
 class Main:
     def __init__(self):
         app = QApplication(sys.argv)
         self.dialog = LoginDialog()
+        self.main_window = None
         self.dialog.token_obtained.connect(self.on_token_obtained)
         sys.exit(app.exec_())
 
     def on_token_obtained(self, token):
-        print(token)
+        self.main_window = MainWindow()
         self.dialog.close()
+        self.main_window.show()
 
 
 if __name__ == '__main__':
