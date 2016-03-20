70c584cfdb20bdb4c913f13c1b979a2926fa9ca8
Mateusz Maćkowski <m4tx@m4tx.pl>
2016-03-20 01:33:59 +0100

    Update Telemetry parser to the new format

--- app/tests/parser/test_telemetry.py
+++ app/tests/parser/test_telemetry.py
@@ -1,16 +1,17 @@
 from app.parser.telemetry import TelemetryParser
 from app.tests.parser import ParserTestCase
 
+TELEMETRY_LINE = ('S,0,0,0,0,0,325e,68c2,6448,3295,3d1,7e,fdd3,d,e83d,e6bd,'
+                  'cdb6,58c,fcbe,995,')
 
-TELEMETRY_LINE = ('S,3a98,146,1ab,64,7fc6,1d06a,6464,1d094,3d2011,1d095,3e,'
-                  'fda0,62,1d096,569,3a1f,1ac6,fcf4,f9b6,f608,1d098')
 TELEMETRY_DATA = {
-    'humidity': 56.38937378,
-    'temperature': 22.05875244,
-    'pressure': 978.00415039,
-    'gyro_x': 0.5425, 'gyro_y': -5.32, 'gyro_z': 0.8575,
-    'accel_x': 0.084485, 'accel_y': 0.907619, 'accel_z': 0.418094,
-    'magnet_x': -0.0624, 'magnet_y': -0.1288, 'magnet_z': -0.20416
+    'error': 0,
+    'humidity': 45.15127563,
+    'temperature': 21.98367676,
+    'pressure': 0.23852539,
+    'gyro_x': 1.1025, 'gyro_y': -4.87375, 'gyro_z': 0.11375,
+    'accel_x': -0.371063, 'accel_y': -0.394487, 'accel_z': -0.785314,
+    'magnet_x': 0.1136, 'magnet_y': -0.06672, 'magnet_z': 0.19624
 }
 
 
