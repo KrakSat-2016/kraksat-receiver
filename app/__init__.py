83906745fb887d7f1be107e206ec5663495dde84
Mateusz Maćkowski <m4tx@m4tx.pl>
2016-03-26 22:13:13 +0100

    Fix invalid stylesheet

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ class Application:
 
         self.q_app = QApplication(sys.argv)
         # Remove ugly frame around Status Bar items on some styles
-        self.q_app.setStyleSheet('QStatusBar::item { border-width: 0px };')
+        self.q_app.setStyleSheet('QStatusBar::item { border-width: 0px }')
         self.dialog = LoginDialog(self.api)
         self.dialog.token_obtained.connect(self._init_app)
         exit_code = self.q_app.exec_()
