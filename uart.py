3332e9b9a6e092cb23f0ee1cfd9433a6ffc2443a
Mateusz Maćkowski <m4tx@m4tx.pl>
2016-04-08 01:54:33 +0200

    Fix uart.py

--- uart.py
+++ uart.py
@@ -1,22 +1,23 @@
-import pyserial
+import serial
+
 
 class Usart:
-    def __init___(self, serial_port, baudrate):
+    def __init__(self, serial_port, baudrate):
         self.uart = serial.Serial(serial_port, baudrate)
 
     def readline(self):
-        self.uart.readline()
+        return self.uart.readline()
 
-usart = Usart("COM4", 57600)
-file = open("data", "a")
-
-while True:
-    try:
-        rawline = usart.readline()
-        line = rawline.decode('utf-8')
-    except UnicodeDecodeError:
-        print(rawline)
-        print("line cannot be decoded\n")
-        continue
-    file.write(line)
-    file.flush()
+if __name__ == '__main__':
+    usart = Usart("/dev/ttyUSB5", 57600)
+    file = open("data", "a")
+    while True:
+        try:
+            rawline = usart.readline()
+            line = rawline.decode('utf-8')
+        except UnicodeDecodeError:
+            print(rawline)
+            print("line cannot be decoded\n")
+            continue
+        file.write(line)
+        file.flush()
